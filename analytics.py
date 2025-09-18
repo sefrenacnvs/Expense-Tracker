6c8d110bf43c4512e3f9f8231ce8a7ead129532c
Sefrena Staci A <sefrenaworks@gmail.com>
2025-09-18 13:40:43 +0530

    analytics.py

--- analytics.py
+++ analytics.py
@@ -1,4 +1,7 @@
 from data_handler import load_data
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
 
 def calculate_summary():
     df = load_data()
@@ -17,3 +20,27 @@ def expenses_by_category():
     if expense_data.empty:
         return None
     return expense_data.groupby('Category')['Amount'].sum()
+
+def train_predict_expenses():
+    df = load_data()
+    if df.empty:
+        return None, None
+
+    df = df[df["Type"] == "Expense"]
+    if df.empty:
+        return None, None
+
+    df["Month"] = pd.to_datetime(df["Date"]).dt.to_period("M").astype(str)
+    monthly = df.groupby("Month")["Amount"].sum().reset_index()
+
+    monthly["MonthIndex"] = np.arange(len(monthly))
+
+    X = monthly[["MonthIndex"]]
+    y = monthly["Amount"]
+    model = LinearRegression()
+    model.fit(X, y)
+
+    next_month = [[len(monthly)]]
+    prediction = model.predict(next_month)[0]
+
+    return monthly, round(prediction, 2)
